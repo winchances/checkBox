d62597e1b9f7125b7376bff6466faa5756727fd8
winchances <409736781@qq.com>
2016-09-25 13:02:54 +0800

    添加适配器和布局文件

--- app/src/main/java/com/example/wenc/listviewcheckboxdemo/MainActivity.java
+++ app/src/main/java/com/example/wenc/listviewcheckboxdemo/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.wenc.listviewcheckboxdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private List<String> list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initData();
+        ListView lv= (ListView) findViewById(R.id.lv);
+
+        MyAdapter adapter=new MyAdapter(this,list);
+        lv.setAdapter(adapter);
+
+    }
+
+    private void initData() {
+        if(list==null){
+            list=new ArrayList<>();
+            for (int i = 0; i <50 ; i++) {
+                list.add("This is a test  "+i);
+            }
+        }
     }
 }
